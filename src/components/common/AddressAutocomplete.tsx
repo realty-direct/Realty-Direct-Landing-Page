594706ad9daab4e52e47a216e75e5665d2654d3a
Josh Thompson <admin@realtydirect.com.au>
2025-06-24 15:52:41 +1000

    Fix deployment again

--- src/components/common/AddressAutocomplete.tsx
+++ src/components/common/AddressAutocomplete.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { TextField, InputAdornment } from '@mui/material';
 import { MapPin } from 'lucide-react';
 
